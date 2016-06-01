f02dcb1a4964e059d8902e8780105e78a0a18676
Noah Klein <noahklein94@gmail.com>
2016-06-01 19:24:10 -0400

    calculator added

---
+++ main.scala
@@ -0,0 +1,21 @@
+import scala.collection.mutable.Stack
+
+
+object Main {
+	def main(args: Array[String]) = {		
+		println(calculate(args))
+	}
+
+	def calculate(tokens: Array[String]): Double = {
+		val stack = new Stack[Double]
+		tokens.foreach(token => token match {
+			case "+" => stack.push(stack.pop() + stack.pop())
+			case "-" => stack.push(stack.pop() - stack.pop())
+			case "*" => stack.push(stack.pop() * stack.pop())
+			case "/" => stack.push(stack.pop() / stack.pop())
+			case "^" => stack.push(Math.pow(stack.pop(), stack.pop()))
+			case _ => stack.push(token.toDouble)
+		})
+		stack.pop()
+	}
+}
