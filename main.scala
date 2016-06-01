ef70c148ed8aad478a55b1ded2999eedda4e24c1
Noah Klein <noahklein94@gmail.com>
2016-06-01 19:26:33 -0400

    fixed indentation

--- main.scala
+++ main.scala
@@ -2,20 +2,20 @@ import scala.collection.mutable.Stack
 
 
 object Main {
-	def main(args: Array[String]) = {		
-		println(calculate(args))
-	}
+  def main(args: Array[String]) = {       
+    println(calculate(args))
+  }
 
-	def calculate(tokens: Array[String]): Double = {
-		val stack = new Stack[Double]
-		tokens.foreach(token => token match {
-			case "+" => stack.push(stack.pop() + stack.pop())
-			case "-" => stack.push(stack.pop() - stack.pop())
-			case "*" => stack.push(stack.pop() * stack.pop())
-			case "/" => stack.push(stack.pop() / stack.pop())
-			case "^" => stack.push(Math.pow(stack.pop(), stack.pop()))
-			case _ => stack.push(token.toDouble)
-		})
-		stack.pop()
-	}
+  def calculate(tokens: Array[String]): Double = {
+    val stack = new Stack[Double]
+    tokens.foreach(token => token match {
+      case "+" => stack.push(stack.pop() + stack.pop())
+      case "-" => stack.push(stack.pop() - stack.pop())
+      case "*" => stack.push(stack.pop() * stack.pop())
+      case "/" => stack.push(stack.pop() / stack.pop())
+      case "^" => stack.push(Math.pow(stack.pop(), stack.pop()))
+      case _ => stack.push(token.toDouble)
+      })
+    stack.pop()
+  }
 }
